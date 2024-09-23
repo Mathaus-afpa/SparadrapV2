f903f69d2e92818a6e2d7de41ec8a93ba088bd50
Mathaus-afpa <mathieu.hoarau@afpa-dev-pompey.fr>
2024-09-23 14:04:25 +0200

    feat(SPARADRAP, Sparadrap.java, CREATE, class): Ecriture de la classe. feat(SPARADRAP, Sparadrap.java, CREATE, function): Ecriture de main(). feat(SPARADRAP, ControleurPrincipal.java, CREATE, class): Ecriture de la classe. feat(SPARADRAP, ControleurPrincipal.java, CREATE, function): Ecriture de entrer(). feat(SPARADRAP, ModelePrincipal.java, CREATE, class): Ecriture de la classe. feat(SPARADRAP, VuePrincipale.java, CREATE, class): Ecriture de la classe. feat(SPARADRAP, VuePrincipale.java, CREATE, function): Ecriture de afficher().

---
+++ src/sparadrap/Sparadrap.java
@@ -0,0 +1,56 @@
+package sparadrap;
+import static sparadrap.mvc.controleurs.ControleurPrincipal.entrer;
+/**
+ * [Sparadrap] - class
+ * @author Mathaus
+ */
+public class Sparadrap {
+	//<editor-fold defaultstate="collapsed" desc="STATIC">
+	//START________________________________________________[static]___________________________________________________//
+	//<editor-fold defaultstate="collapsed" desc="PUBLIC">
+    //<editor-fold defaultstate="collapsed" desc="Proprietes PUBLIC">
+    //</editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="Fonctions PUBLIC">
+	public static void main(String[] args) {
+		entrer();
+	}
+    //</editor-fold>
+	//</editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="PRIVATE">
+    //<editor-fold defaultstate="collapsed" desc="Proprietes PRIVATE">
+    //</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="Fonctions PRIVATE">
+    //</editor-fold>
+	//</editor-fold>
+    //END//////////////////////////////////////////////////[static]/////////////////////////////////////////////////////
+	//</editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="INSTANCE">
+    //START_______________________________________________[instance]__________________________________________________//
+    // <editor-fold defaultstate="collapsed" desc="CONSTRUCTEURS">
+	private Sparadrap() {}
+    // </editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="PUBLIC">
+    //<editor-fold defaultstate="collapsed" desc="Attributs PUBLIC">
+    //</editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="Methodes PUBLIC">
+    //</editor-fold>
+	//</editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="PRIVATE">
+    //<editor-fold defaultstate="collapsed" desc="Attributs PRIVATE">
+    //</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="Methodes PRIVATE">
+    //</editor-fold>
+	//</editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="ENCAPSULATION">
+    //<editor-fold defaultstate="collapsed" desc="Getters">
+    //</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="Setters">
+    //</editor-fold>
+	//</editor-fold>
+    //END/////////////////////////////////////////////////[instance]////////////////////////////////////////////////////
+	//</editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="OVERRIDE">
+	//START_______________________________________________[override]__________________________________________________//
+	//END/////////////////////////////////////////////////[override]////////////////////////////////////////////////////
+	//</editor-fold>
+}
---
+++ src/sparadrap/mvc/controleurs/ControleurPrincipal.java
@@ -0,0 +1,59 @@
+package sparadrap.mvc.controleurs;
+import static sparadrap.mvc.vues.VuePrincipale.afficher;
+/**
+ * [ControleurPrincipal] - class
+ * @author Mathaus
+ */
+public class ControleurPrincipal {
+	//<editor-fold defaultstate="collapsed" desc="STATIC">
+	//START________________________________________________[static]___________________________________________________//
+	//<editor-fold defaultstate="collapsed" desc="PUBLIC">
+    //<editor-fold defaultstate="collapsed" desc="Proprietes PUBLIC">
+    //</editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="Fonctions PUBLIC">
+	/**
+	 * Affiche la vue.
+	 */
+	public static final void entrer() {
+		afficher();
+	}
+    //</editor-fold>
+	//</editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="PRIVATE">
+    //<editor-fold defaultstate="collapsed" desc="Proprietes PRIVATE">
+    //</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="Fonctions PRIVATE">
+    //</editor-fold>
+	//</editor-fold>
+    //END//////////////////////////////////////////////////[static]/////////////////////////////////////////////////////
+	//</editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="INSTANCE">
+    //START_______________________________________________[instance]__________________________________________________//
+    // <editor-fold defaultstate="collapsed" desc="CONSTRUCTEURS">
+	private ControleurPrincipal() {}
+    // </editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="PUBLIC">
+    //<editor-fold defaultstate="collapsed" desc="Attributs PUBLIC">
+    //</editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="Methodes PUBLIC">
+    //</editor-fold>
+	//</editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="PRIVATE">
+    //<editor-fold defaultstate="collapsed" desc="Attributs PRIVATE">
+    //</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="Methodes PRIVATE">
+    //</editor-fold>
+	//</editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="ENCAPSULATION">
+    //<editor-fold defaultstate="collapsed" desc="Getters">
+    //</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="Setters">
+    //</editor-fold>
+	//</editor-fold>
+    //END/////////////////////////////////////////////////[instance]////////////////////////////////////////////////////
+	//</editor-fold>
+	//<editor-fold defaultstate="collapsed" desc="OVERRIDE">
+	//START_______________________________________________[override]__________________________________________________//
+	//END/////////////////////////////////////////////////[override]////////////////////////////////////////////////////
+	//</editor-fold>
+}
