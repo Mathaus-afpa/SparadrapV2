d9e649461a98f554714ca83d565757a5e74cd58e
Mathaus-afpa <mathieu.hoarau@afpa-dev-pompey.fr>
2024-09-24 16:24:32 +0200

    ressource(SPARADRAP, AccueilBG.png, CREATE, image): Ajout de AccueilBG.png. feat(SPARADRAP, PageAccueil.java, CREATE, method): Ajout de creerPanneauGauche() et de creerPanneauDroit(). usefeat(SPARADRAP, PageAccueil.java, UPDATE, constructor): Modification du constructeur.

--- src/sparadrap/mvc/vues/pages/PageAccueil.java
+++ src/sparadrap/mvc/vues/pages/PageAccueil.java
@@ -1,5 +1,10 @@
 package sparadrap.mvc.vues.pages;
 import sparadrap.composants.sparadrap.SparadrapPage;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 /**
  * [PageAccueil] - class
  * @author Mathaus
@@ -43,7 +48,9 @@ public class PageAccueil extends SparadrapPage {
 		if (SingletonPageAccueil.INSTANCE != null) {
 			throw new IllegalStateException("Instance already created");
 		}
-		//todo: designer Page.
+		creerPanneauGauche();
+		creerPanneauDroit();
+		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 		this.setVisible(true);
 	}
 	// </editor-fold>
@@ -57,6 +64,46 @@ public class PageAccueil extends SparadrapPage {
 	//<editor-fold defaultstate="expanded" desc="Attributs PRIVATE">
 	//</editor-fold>
 	//<editor-fold defaultstate="expanded" desc="Methodes PRIVATE">
+	/**
+	 * Creer le panneau de gauche avec le logo en fond.
+	 */
+	private void creerPanneauGauche() {
+		JPanel panneauGauche = new JPanel() {
+			private BufferedImage backgroundImage;
+			{
+				try {
+					backgroundImage = ImageIO.read(getClass().getResource("/sparadrap/ressources/AccueilBG.png"));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			// Surcharger la méthode paintComponent pour dessiner l'image de fond.
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				if (backgroundImage != null) {
+					int width = getWidth();
+					int height = getHeight();
+					g.drawImage(backgroundImage, 0, 0, width, height, this);
+				}
+			}
+		};
+		// Fixer taille et couleur du panel en rapport a l'image.
+		Dimension dimensionImage = new Dimension(487, 590);
+		panneauGauche.setMinimumSize(dimensionImage);
+		panneauGauche.setPreferredSize(dimensionImage);
+		panneauGauche.setMaximumSize(dimensionImage);
+		this.setBackground(new Color(255, 174, 201)); // Couleur du fond de l'image.
+		this.add(panneauGauche);
+	}
+	/***
+	 * Creer le panneau de droite avec les boutons.
+	 */
+	private void creerPanneauDroit() {
+		JPanel panneauDroit = new JPanel();
+		panneauDroit.setBackground(Color.BLACK); // Permet d'ajuster le pixel de la bordure.
+		this.add(panneauDroit);
+	}
 	//</editor-fold>
 	//</editor-fold>
 	//<editor-fold defaultstate="expanded" desc="ENCAPSULATION">
