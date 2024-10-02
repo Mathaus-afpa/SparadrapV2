27cae3fce86a91c534e66fa924b9983dab8b0f97
Mathaus-afpa <mathieu.hoarau@afpa-dev-pompey.fr>
2024-10-02 15:10:25 +0200

    ressource(SPARADRAP, AccueilBouton.png, CREATE, image): Ajout de AccueilBouton.png. feat(SPARADRAP, Util.java, CREATE, class): Ecriture de la classe. feat(SPARADRAP, Util.java, CREATE, function): Ecriture de ajouterPanneauAccueil() et de creerIconePourBoutonAccueil(). feat(SPARADRAP, Util.java, CREATE, constante): Definition de ICON_ACCUEIL, de BTN_ACCUEIL_WIDTH et de BTN_ACCUEIL_HEIGHT. usefeat(SPARADRAP, PageMedecins.java, UPDATE, method): Modification de designerPageMedecins(). feat(SPARADRAP, PageMedecins.java, CREATE, method): Ecriture de designerPanneauSud().

--- src/sparadrap/composants/Util.java
+++ src/sparadrap/composants/Util.java
@@ -1,17 +1,15 @@
-package sparadrap.mvc.vues.pages;
-import sparadrap.composants.Util;
+package sparadrap.composants;
 import sparadrap.composants.designers.Designer;
-import sparadrap.composants.sparadrap.SparadrapPage;
-import sparadrap.composants.sparadrap.SparadrapPanneau;
-import sparadrap.composants.sparadrap.SparadrapVue;
-import sparadrap.mvc.vues.metiers.VueMedecin;
+import sparadrap.composants.enums.PAGES;
+import sparadrap.mvc.vues.pages.ManagerDesPages;
 import javax.swing.*;
-import static sparadrap.mvc.modeles.ModelePrincipal.*;
+import java.awt.*;
+import java.util.Objects;
 /**
- * [PageMedecin] - class
+ * [Util] - class
  * @author Mathaus
  */
-public class PageMedecins extends SparadrapPage {
+public class Util {
 	//<editor-fold defaultstate="expanded" desc="STATIC">
 	//START________________________________________________[static]___________________________________________________//
 	//<editor-fold defaultstate="expanded" desc="PUBLIC">
@@ -19,39 +17,54 @@ public class PageMedecins extends SparadrapPage {
 	//</editor-fold>
 	//<editor-fold defaultstate="expanded" desc="Fonctions PUBLIC">
 	/**
-	 * Recupere l'instance unique du Singleton.
-	 * @return (SparadrapPage)
+	 * Creer le panneau du bouton d'accueil.
+	 * @return (JPanel)
 	 */
-	public static SparadrapPage getInstance() {
-		return SingletonPageMedecins.INSTANCE;
+	public static JPanel ajouterPanneauAccueil() {
+		JPanel colonneBoutonAccueil = new JPanel();
+		JButton boutonAccueil = new JButton(ICON_ACCUEIL);
+		Dimension imageDimension = new Dimension(BTN_ACCUEIL_WIDTH, BTN_ACCUEIL_HEIGHT);
+		boutonAccueil.setMinimumSize(imageDimension);
+		boutonAccueil.setPreferredSize(imageDimension);
+		boutonAccueil.setMaximumSize(imageDimension);
+		boutonAccueil.setBorder(null);
+		colonneBoutonAccueil.setMinimumSize(imageDimension);
+		colonneBoutonAccueil.setPreferredSize(imageDimension);
+		colonneBoutonAccueil.setMaximumSize(imageDimension);
+		colonneBoutonAccueil.add(boutonAccueil);
+		Designer.definirUneMiseEnPageSpring(colonneBoutonAccueil, boutonAccueil, null);
+		boutonAccueil.addActionListener(onclick -> ManagerDesPages.afficherPage(PAGES.ACCUEIL));
+		return colonneBoutonAccueil;
 	}
 	//</editor-fold>
 	//</editor-fold>
 	//<editor-fold defaultstate="expanded" desc="PRIVATE">
 	//<editor-fold defaultstate="expanded" desc="Proprietes PRIVATE">
+	private static final ImageIcon ICON_ACCUEIL = creerIconePourBoutonAccueil();
+	private final static int BTN_ACCUEIL_WIDTH = 200;
+	private final static int BTN_ACCUEIL_HEIGHT = 64;
 	//</editor-fold>
 	//<editor-fold defaultstate="expanded" desc="Fonctions PRIVATE">
+	/**
+	 * Recreer l'image du bouton accueil dans une taille coherente.
+	 * @return (ImageIcon)
+	 */
+	private static ImageIcon creerIconePourBoutonAccueil() {
+		ImageIcon iconeDepuisImage = new ImageIcon(Objects.requireNonNull(Util.class.getResource("/sparadrap/ressources/AccueilBouton.png")));
+		Image imageTailleReelle = iconeDepuisImage.getImage();
+		Image fixerDimensionsImage = imageTailleReelle.getScaledInstance(BTN_ACCUEIL_WIDTH, BTN_ACCUEIL_HEIGHT, Image.SCALE_SMOOTH);
+		return new ImageIcon(fixerDimensionsImage);
+	}
 	//</editor-fold>
 	//</editor-fold>
 	//END//////////////////////////////////////////////////[static]/////////////////////////////////////////////////////
 	//</editor-fold>
 	//<editor-fold defaultstate="expanded" desc="INSTANCE">
 	//START_______________________________________________[instance]__________________________________________________//
-	// <editor-fold defaultstate="expanded" desc="SINGLETON">
-	/**
-	 * Classe porteuse du Singleton.
-	 */
-	private static final class SingletonPageMedecins {
-		private static final PageMedecins INSTANCE = new PageMedecins();
-	}
+	//<editor-fold defaultstate="expanded" desc="SINGLETON">
 	//</editor-fold>
 	//<editor-fold defaultstate="expanded" desc="CONSTRUCTEURS">
-	private PageMedecins() {
-		if (SingletonPageMedecins.INSTANCE != null) {
-			throw new IllegalStateException("Instance already created");
-		}
-		designerPageMedecins();
-	}
+	private Util() {}
 	//</editor-fold>
 	//<editor-fold defaultstate="expanded" desc="PUBLIC">
 	//<editor-fold defaultstate="expanded" desc="Attributs PUBLIC">
@@ -63,33 +76,6 @@ public class PageMedecins extends SparadrapPage {
 	//<editor-fold defaultstate="expanded" desc="Attributs PRIVATE">
 	//</editor-fold>
 	//<editor-fold defaultstate="expanded" desc="Methodes PRIVATE">
-	/**
-	 * Ajoute les differents panneaux et leur design.
-	 */
-	private void designerPageMedecins() {
-		this.setPanneauNord(new SparadrapPanneau(), BANDEAU_HAUT_TAILLE);
-		this.setPanneauCentre(new SparadrapPanneau());
-		designerPanneauCentre();
-		this.setPanneauSud(new SparadrapPanneau(), BANDEAU_BAS_TAILLE);
-		designerPanneauSud();
-	}
-	/**
-	 * Ajoute les elements du panneau centre.
-	 */
-	private void designerPanneauCentre() {
-		SparadrapPanneau centre = this.getPanneauCentre();
-		SparadrapVue vue = new VueMedecin(false);
-		centre.add(vue);
-		Designer.definirUneMiseEnPageSpring(centre, vue, new int[] {0,120,0,40});
-	}
-	/**
-	 * Ajoute les elements du panneau sud.
-	 */
-	private void designerPanneauSud() {
-		SparadrapPanneau sud = this.getPanneauSud();
-		sud.setLayout(new BoxLayout(sud, BoxLayout.X_AXIS));
-		sud.add(Util.ajouterPanneauAccueil());
-	}
 	//</editor-fold>
 	//</editor-fold>
 	//<editor-fold defaultstate="expanded" desc="ENCAPSULATION">
